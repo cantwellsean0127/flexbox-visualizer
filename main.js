178e69b9d4a6e320c965148676f48cdf45faeb91
cantwellsean0127 <103895364+cantwellsean0127@users.noreply.github.com>
2023-10-29 21:50:25 -0500

    Drastically improved code readability and maintainability.

--- main.js
+++ main.js
@@ -1,84 +1,50 @@
+// Grabs various elements from the document that will be used throughout the script
 const controls = document.getElementById("controls")
 const visual = document.getElementById("visual")
 const boxes = visual.getElementsByTagName("div")
 
-const createRules = () => {
-    const rules = {
-        "display": { values: ["block", "inline", "flex"], default_value: "block" },
-        "flex-direction": { values: ["row", "column", "row-reverse", "column-reverse"], default_value: "row" },
-        "justify-content": { values: ["flex-start", "flex-end", "center", "space-between", "space-around", "space-evenly"], default_value: "flex-start" },
-        "align-items": { values: ["flex-start", "flex-end", "center", "stretch", "baseline"], default_value: "flex-start" },
-        "flex-wrap": { values: ["nowrap", "wrap", "wrap-reverse"], default_value: "nowrap" }
-    }
-    const selectorOpening = document.createElement("p")
-    selectorOpening.setAttribute("class", "text-orange-600")
-    selectorOpening.textContent = "#visual {"
-    controls.appendChild(selectorOpening)
-    for (const rule in rules) {
-        const container = document.createElement("div")
-        container.setAttribute("id", rule)
-        const label = document.createElement("label")
-        label.setAttribute("for", rule + "Selector")
-        label.setAttribute("class", "text-white ml-8")
-        label.textContent = rule + ":"
-        container.appendChild(label)
-        const select = document.createElement("select")
-        select.setAttribute("name", rule)
-        select.setAttribute("id", rule + "Selector")
-        select.setAttribute("class", "bg-slate-900 text-cyan-600 m-2 w-fit")
-        for (const value of rules[rule].values) {
-            const option = document.createElement("option")
-            option.setAttribute("value", value)
-            option.textContent = value
-            if (value === rule.default_value) {
-                option.setAttribute("selected", "")
-            }
-            select.appendChild(option)
-        }
-        select.addEventListener("change", (event) => {
-            visual.style.setProperty(rule, event.target.value)
-        })
-        container.appendChild(select)
-        controls.appendChild(container)
-    }
-    const selectorClosing = document.createElement("p")
-    selectorClosing.textContent = "}"
-    selectorClosing.setAttribute("class", "text-orange-600")
-    controls.appendChild(selectorClosing)
-
-}
-
+// This function adds a new display box (like the array.push method)
 const addDisplayBox = (numberOfDisplayBoxesToAdd) => {
-    if (numberOfDisplayBoxesToAdd === undefined) {
-        numberOfDisplayBoxesToAdd = 1
-    }
 
+    // If the number of display boxes to create was not passed, manually set the value to 1
+    if (numberOfDisplayBoxesToAdd === undefined) numberOfDisplayBoxesToAdd = 1
+
+    // Creates N amount of boxes
     for (let boxCount = 0; boxCount < numberOfDisplayBoxesToAdd; boxCount++) {
-        const colors = ["red", "orange", "amber", "yellow", "lime", "green", "emerald", "teal", "cyan", "sky", "blue", "indigo", "violet", "purple", "fuchsia", "pink", "rose"]
+
+        // These are the various colors that our display box can be. They are all from Tailwind CSS, however, Tailwind CSS does have more options that are not here. The unincluded options are black, white, slate, gray, zinc, neutral, and stone.
+        const colors = "red orange amber yellow lime green emerald teal cyan sky blue indigo violet purple fuchsia pink rose".split(" ")
+
+        // Gets a random color from the list of colors above
         const randomColor = colors[Math.floor(Math.random() * colors.length)]
+
+        // Creates a div element to be the main container for the box
         const box = document.createElement("div")
-        box.className = `flex justify-center items-center border-8 border-${randomColor}-500 h-fit w-fit bg-${randomColor}-200 p-5`
-        const boxText = document.createElement("h1")
-        boxText.textContent = `Box ${boxes.length + 1}`
-        boxText.className = `text-${randomColor}-500 text-lg font-bold select-none`
-        boxText.addEventListener("click", (event) => {
-            event.stopPropagation()
-            createBoxInformationModal(event.target.parentNode)
-        })
-        box.appendChild(boxText)
-        box.addEventListener("click", (event) => {
-            createBoxInformationModal(event.target)
-        })
         visual.appendChild(box)
+
+        // Sets the styling for the main box container
+        box.className = `flex justify-center items-center border-8 border-${randomColor}-500 h-fit w-fit bg-${randomColor}-200 p-5`
+
+        // Creates and styles an h1 element to be the text inside the box
+        const boxText = document.createElement("h1")
+        box.appendChild(boxText)
+        boxText.textContent = `Box ${boxes.length}`
+        boxText.className = `text-${randomColor}-500 text-lg font-bold select-none`
+
+        // Adds an event listener so that when the box is clicked, it creates the box details modal
+        box.addEventListener("click", (event) => {
+            if (event.target.tagName === "DIV") createBoxInformationModal(event.target)
+            else if (event.target.tagName === "H1") createBoxInformationModal(event.target.parentNode)
+        })
     }
 }
 
+// This function removes the most recently added display box (like the array.pop method)
 const removeDisplayBox = () => {
-    if (boxes.length > 0) {
-        visual.removeChild(boxes[boxes.length - 1])
-    }
+    if (boxes.length > 0) visual.removeChild(boxes[boxes.length - 1])
 }
 
+// This function adds an event listener so that when a user presses enter or delete, a box is added or removed respectively. We also want to make sure the target is the body. Otherwise when a user is editing an individual box's flex grow or flex shrink property, they could press enter and/or backspace while typing and trigger this event.
 document.addEventListener("keydown", (event) => {
     if (event.key === "Backspace" && event.target === document.body) {
         removeDisplayBox()
@@ -87,119 +53,203 @@ document.addEventListener("keydown", (event) => {
     }
 })
 
+// This function adds event listeners so that whenever a property value is updated on the controls, the respective property is updated on the display as well. This uses standard CSS and not Tailwind CSS.
+const addRuleEventListeners = () => {
+    const selects = document.getElementsByTagName("select")
+    for (const select of selects) {
+        const property = select.id.slice(0, select.id.length - 9)
+        select.addEventListener("change", event => visual.style.setProperty(property, event.target.value))
+    }
+}
+
+// This function creates a modal that displays information about an individual box
 const createBoxInformationModal = (box) => {
+
+    // Grabs the boxes header, this will be used multiple times later on.
     const boxHeader = box.getElementsByTagName("h1")[0]
+
+    // Grabs the boxes styling, this will also be used multiple times later on.
     const boxStyles = window.getComputedStyle(box)
+
+    // Creates and styles the modal overlay.
     const modalOverlay = document.createElement("div")
+    document.body.appendChild(modalOverlay)
     modalOverlay.id = "boxInformationModalOverlay"
     modalOverlay.className = "fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center"
+
+    // Creates and styles the modal content container.
     const modalContent = document.createElement("div")
-    modalContent.className = "bg-white rounded-lg shadow-lg w-1/3 p-6"
     modalOverlay.appendChild(modalContent)
+    modalContent.className = "bg-white rounded-lg shadow-lg w-1/3 p-6"
+
+    // Creates and styles the header for the modal
     const modalHeader = document.createElement("h1")
+    modalContent.appendChild(modalHeader)
     modalHeader.className = "text-xl font-bold mb-4"
     modalHeader.textContent = `${box.textContent} Details`
-    modalContent.appendChild(modalHeader)
+
+    // Creates and styles the text size details container.
     const textSizeDetails = document.createElement("div")
+    modalContent.appendChild(textSizeDetails)
     textSizeDetails.className = "mb-4"
+
+    // Creates and styles the text size label
     const textSizeLabel = document.createElement("label")
-    textSizeLabel.setAttribute("for", "boxColorSelector")
+    textSizeDetails.appendChild(textSizeLabel)
+    textSizeLabel.for = "boxTextSizeSelector"
     textSizeLabel.className = "block text-sm font-medium text-gray-700 mb-2"
     textSizeLabel.textContent = "Text Size:"
-    textSizeDetails.appendChild(textSizeLabel)
+
+    // Creates and styles the text size selector
     textSizeSelector = document.createElement("select")
+    textSizeDetails.appendChild(textSizeSelector)
     textSizeSelector.id = "boxColorSelector"
     textSizeSelector.title = "boxColorSelector"
     textSizeSelector.className = "border rounded-md p-2 w-full text-gray-700"
-    const textSizes = ["sm", "md", "lg", "xl", "2xl", "3xl", "4xl", "5xl", "6xl", "7xl", "8xl", "9xl",]
-    let currentTextSize = [...boxHeader.classList].filter((classValue) => classValue.startsWith("text-") && classValue.length <= 8)[0]
-    currentTextSize = currentTextSize.slice(5)
+
+    // These are the various sizes that our box text can be. They are all from Tailwind CSS.
+    const textSizes = "text-sm text-md text-lg text-xl text-2xl text-3xl text-4xl text-5xl text-6xl text-7xl text-8xl text-9xl".split(" ")
+
+    // Gets the current text size
+    let currentTextSize = [...boxHeader.classList].filter((className) => textSizes.includes(className))[0]
+
+    // Creates the different options for the text size
     for (const textSize of textSizes) {
+
+        // Creates this option
         const textSizeOption = document.createElement("option")
-        textSizeOption.value = textSize
-        textSizeOption.textContent = textSize.toUpperCase()
-        if (textSize === currentTextSize) {
-            textSizeOption.setAttribute("selected", "")
-        }
         textSizeSelector.appendChild(textSizeOption)
+        textSizeOption.value = textSize
+        textSizeOption.textContent = textSize.slice(5).toUpperCase()
+
+        // If this option is the current text size, set it as the defaultly selected option
+        if (textSize === currentTextSize) textSizeOption.setAttribute("selected", "")
     }
+
+    // Adds and event listener so that whenever the text size is updated on the selector, the respective class is added to the box
     textSizeSelector.addEventListener("change", (event) => {
-        for (const textSize of textSizes) {
-            boxHeader.classList.remove(`text-${textSize}`)
-        }
+
+        // Removes the previous text size class
+        for (const textSize of textSizes) boxHeader.classList.remove(`text-${textSize}`)
+
+        // Adds the newly selected text size class
         boxHeader.classList.add(`text-${event.target.value}`)
     })
-    textSizeDetails.appendChild(textSizeSelector)
-    modalContent.appendChild(textSizeDetails)
+
+    // Creates and styles the box color details container.
     const boxColorDetails = document.createElement("div")
+    modalContent.appendChild(boxColorDetails)
     boxColorDetails.className = "mb-4"
+
+    // Creates and styles the box color label
     const boxColorLabel = document.createElement("label")
-    boxColorLabel.setAttribute("for", "boxColorSelector")
+    boxColorDetails.appendChild(boxColorLabel)
+    boxColorLabel.for = boxColorSelector
     boxColorLabel.className = "block text-sm font-medium text-gray-700 mb-2"
     boxColorLabel.textContent = "Color:"
-    boxColorDetails.appendChild(boxColorLabel)
+
+    // Creates and styles the box color selector
     boxColorSelector = document.createElement("select")
+    boxColorDetails.appendChild(boxColorSelector)
     boxColorSelector.id = "boxColorSelector"
     boxColorSelector.title = "boxColorSelector"
     boxColorSelector.className = "border rounded-md p-2 w-full text-gray-700"
-    const colors = ["red", "orange", "amber", "yellow", "lime", "green", "emerald", "teal", "cyan", "sky", "blue", "indigo", "violet", "purple", "fuchsia", "pink", "rose"]
+
+    // These are the various colors that our display box can be. They are all from Tailwind CSS, however, Tailwind CSS does have more options that are not here. The unincluded options are black, white, slate, gray, zinc, neutral, and stone.
+    const colors = "red orange amber yellow lime green emerald teal cyan sky blue indigo violet purple fuchsia pink rose".split(" ")
+
+    // Gets the current box color
     let currentBoxColor = [...box.classList].filter((classValue) => classValue.startsWith("bg-"))[0]
     currentBoxColor = currentBoxColor.slice(3, currentBoxColor.length - 4)
+
+    // Creates the different options for color
     for (const color of colors) {
+
+        // Creates this specific option
         const boxColorOption = document.createElement("option")
+        boxColorSelector.appendChild(boxColorOption)
         boxColorOption.value = color
         boxColorOption.textContent = `${color[0].toUpperCase()}${color.slice(1)}`
-        if (color === currentBoxColor) {
-            boxColorOption.setAttribute("selected", "")
-        }
-        boxColorSelector.appendChild(boxColorOption)
+
+        // If this option is the current text size, set it as the defaultly selected option
+        if (color === currentBoxColor) boxColorOption.setAttribute("selected", "")
     }
+
+    // Adds and event listener so that whenever the color is updated on the selector, the respective class is added to the box
     boxColorSelector.addEventListener("change", (event) => {
+
+        // Removes the previous border color class and adds the newly selected border color class
         box.classList.remove(`border-${currentBoxColor}-500`)
         box.classList.add(`border-${event.target.value}-500`)
+
+        // Removes the previous background color class and adds the newly selected background color class
         box.classList.remove(`bg-${currentBoxColor}-200`)
         box.classList.add(`bg-${event.target.value}-200`)
+
+        // Removes the previous text color class and adds the newly selected text color class
         boxHeader.classList.remove(`text-${currentBoxColor}-500`)
         boxHeader.classList.add(`text-${event.target.value}-500`)
     })
-    boxColorDetails.appendChild(boxColorSelector)
-    modalContent.appendChild(boxColorDetails)
+
+    // Creates the flex grow and flex shrink details. These are exactly the same except for the name (shrink/grow).
     const growShrink = ["grow", "shrink"]
     growShrink.forEach((property) => {
+
+        // Creates and styles the flex grow/shrink details container.
         const growShrinkValueDetail = document.createElement("div")
-        growShrinkValueDetail.className = "mb-4"
-        const growValueDetailLabel = document.createElement("label")
-        growValueDetailLabel.setAttribute("for", `${property}ValueDetailNumber`)
-        growValueDetailLabel.className = "block text-sm font-medium text-gray-700 mb-2"
-        growValueDetailLabel.textContent = "Grow Value:"
-        growShrinkValueDetail.appendChild(growValueDetailLabel)
-        const growValueDetailNumber = document.createElement("input")
-        growValueDetailNumber.type = "number"
-        growValueDetailNumber.id = `${property}ValueDetailNumber`
-        growValueDetailNumber.name = `${property}ValueDetailNumber`
-        growValueDetailNumber.className = "border rounded-md p-2 w-full text-gray-700"
-        const currentBoxGrowValue = boxStyles.getPropertyValue(`flex-${property}`)
-        growValueDetailNumber.placeholder = currentBoxGrowValue
-        growValueDetailNumber.min = "0"
-        growValueDetailNumber.addEventListener("change", (event) => {
-            box.style[`flex-${property}`] = "" + event.target.value
-        })
-        growShrinkValueDetail.appendChild(growValueDetailNumber)
         modalContent.appendChild(growShrinkValueDetail)
+        growShrinkValueDetail.className = "mb-4"
+
+        // Creates and styles the flex grow/shrink label.
+        const growShrinkValueDetailLabel = document.createElement("label")
+        growShrinkValueDetail.appendChild(growShrinkValueDetailLabel)
+        growShrinkValueDetailLabel.for = `${property}ValueDetailNumber`
+        growShrinkValueDetailLabel.className = "block text-sm font-medium text-gray-700 mb-2"
+        growShrinkValueDetailLabel.textContent = `Flex ${property[0].toUpperCase()}${property.slice(1)}`
+
+        // Creates and styles the flex shrink/grow number input
+        const growShrinkValueDetailNumber = document.createElement("input")
+        growShrinkValueDetail.appendChild(growShrinkValueDetailNumber)
+        growShrinkValueDetailNumber.type = "number"
+        growShrinkValueDetailNumber.id = `${property}ValueDetailNumber`
+        growShrinkValueDetailNumber.name = `${property}ValueDetailNumber`
+        growShrinkValueDetailNumber.className = "border rounded-md p-2 w-full text-gray-700"
+
+        // Gets the current flex grow/shrink values
+        const currentBoxGrowValue = boxStyles.getPropertyValue(`flex-${property}`)
+
+        // Sets the placeholder to be the current flex grow/shrink value
+        growShrinkValueDetailNumber.placeholder = currentBoxGrowValue
+
+        // Sets the minimum value to be 0.
+        growShrinkValueDetailNumber.min = "0"
+
+        // Adds and event listener so that whenever the number is updated, the respective class is added to the box
+        growShrinkValueDetailNumber.addEventListener("change", event => box.style[`flex-${property}`] = "" + event.target.value)
     })
-    document.body.appendChild(modalOverlay)
+
+    // Adds an event listener so that when the user clicks off the modal, it gets removed
     document.body.addEventListener("click", removeBoxInformationModal)
 }
 
+// This function removes the modal that displays information about an individual box
 const removeBoxInformationModal = (event) => {
     const boxInformationModalOverlay = document.getElementById("boxInformationModalOverlay")
-    if (event.target !== boxInformationModalOverlay) {
-        return
-    } else {
+    if (event.target === boxInformationModalOverlay) {
         document.body.removeChild(boxInformationModalOverlay)
         document.body.removeEventListener("click", removeBoxInformationModal)
     }
 }
 
-createRules()
-addDisplayBox(3)
+// This function is the start of this script's control flow
+const start = () => {
+
+    // Adds 3 starter boxes
+    addDisplayBox(3)
+
+    // Adds the event listeners for the controls on the left
+    addRuleEventListeners()
+}
+
+// Starts this script's execution (like a main function)
+start()
